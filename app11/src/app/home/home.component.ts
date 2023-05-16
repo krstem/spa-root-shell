1d2cc5086509d28b28f1a2311934242e4242f34c
anetastankovska <aneta.stankovska@sourcico.com>
2023-05-16 13:21:07 +0200

    add ui

--- app11/src/app/home/home.component.ts
+++ app11/src/app/home/home.component.ts
@@ -8,4 +8,21 @@ import { assetUrl } from 'src/single-spa/asset-url';
 })
 export class HomeComponent {
   url = assetUrl('code-loader.gif');
+
+  products: any[] = [
+    {
+      name: 'Product 1',
+      description: 'This is the description of Product 1.',
+      price: 19.99,
+      image: 'path/to/product1-image.jpg'
+    },
+    {
+      name: 'Product 2',
+      description: 'This is the description of Product 2.',
+      price: 24.99,
+      image: 'path/to/product2-image.jpg'
+    },
+    // Add more products here
+  ];
+
 }
